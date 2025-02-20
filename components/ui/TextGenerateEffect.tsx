3046be4b972724ed15c9aa490f94d6ed40031057
code3-dev <h3dev.pira@gamil.com>
2025-02-20 23:45:19 +0330

    first publish

--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { useEffect } from "react";
-import { motion, stagger, useAnimate } from "framer-motion";
+import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 
 export const TextGenerateEffect = ({
@@ -14,48 +13,38 @@ export const TextGenerateEffect = ({
   filter?: boolean;
   duration?: number;
 }) => {
-  const [scope, animate] = useAnimate();
   let wordsArray = words.split(" ");
-  useEffect(() => {
-    animate(
-      "span",
-      {
-        opacity: 1,
-        filter: filter ? "blur(0px)" : "none",
-      },
-      {
-        duration: duration ? duration : 1,
-        delay: stagger(0.2),
-      }
-    );
-  }, [scope.current]);
-
-  const renderWords = () => {
-    return (
-      <motion.div ref={scope}>
-        {wordsArray.map((word, idx) => {
-          return (
-            <motion.span
-              key={word + idx}
-              className={`${idx > 3 ? 'text-purple' : 'dark:text-white text-black'} opacity-0`}
-              style={{
-                filter: filter ? "blur(10px)" : "none",
-              }}
-            >
-              {word}{" "}
-            </motion.span>
-          );
-        })}
-      </motion.div>
-    );
-  };
 
   return (
     <div className={cn("font-bold", className)}>
       <div className="my-4">
-        <div className="dark:text-white text-black leading-snug tracking-wide">
-          {renderWords()}
-        </div>
+        <motion.div
+          className="dark:text-white text-black leading-snug tracking-wide"
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: { opacity: 0 },
+            visible: {
+              opacity: 1,
+              transition: {
+                staggerChildren: 0.2,
+              },
+            },
+          }}
+        >
+          {wordsArray.map((word, idx) => (
+            <motion.span
+              key={word + idx}
+              className={`${idx > 3 ? 'text-purple' : 'dark:text-white text-black'}`}
+              variants={{
+                hidden: { opacity: 0, filter: filter ? "blur(10px)" : "none" },
+                visible: { opacity: 1, filter: "blur(0px)", transition: { duration } },
+              }}
+            >
+              {word}{" "}
+            </motion.span>
+          ))}
+        </motion.div>
       </div>
     </div>
   );
